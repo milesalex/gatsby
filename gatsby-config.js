6de5fc13e8ee2f89e49b486593d6790c402df3e8
Alex Miles <alex@tokentax.us>
2019-11-15 14:14:30 -0500

    Use preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   // host: process.env.CONTENTFUL_HOST
+  host: `preview.contentful.com`,
 }
 
 module.exports = {
